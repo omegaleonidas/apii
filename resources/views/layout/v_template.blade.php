4415cd7ffb747669dfa1f6e77c70855ab66333f4
hidayatul sidiq <39238118+omegaleonidas@users.noreply.github.com>
2021-10-03 00:22:11 +0700

    foto

--- resources/views/layout/v_template.blade.php
+++ resources/views/layout/v_template.blade.php
@@ -74,7 +74,7 @@
       <!-- Sidebar user (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="{{asset('template/')}}/dist/img/user1.PNG" class="img-circle elevation-2" alt="User Image">
+          <img src="{{asset('template/')}}/dist/img/user1.png" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">{{Auth::user()->name}}</a>

0654977fc01a679601a7e2e214dd44a543af2c18
hidayatul sidiq <39238118+omegaleonidas@users.noreply.github.com>
2021-10-03 00:54:52 +0700

    print

--- app/Http/Controllers/controllersUser/PrintController1.php
+++ app/Http/Controllers/controllersUser/PrintController1.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\controllersUser;
 
 
 use Illuminate\Http\Request;
-use App\Models\Absensimodel;
+use App\Models\AbsensiModel;
 use App\Http\Controllers\Controller;
 use Dompdf\Dompdf;
 use Illuminate\Support\Facades\DB;

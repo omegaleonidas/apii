cb1300bf1880b749d19454392f74210ee91132ed
omegaleonidas <omegaleonidas33@gmail.com>
2021-10-02 22:33:32 +0700

    api php

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
-use App\Http\Controllers\pegawaiController; 
+use App\Http\Controllers\PegawaiController; 
 use App\Http\Controllers\ApiPegawai;
 use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\cutiController;
-use App\Http\Controllers\jabatanController;
-use App\Http\Controllers\informasiController;
+use App\Http\Controllers\CutiController;
+use App\Http\Controllers\JabatanController;
+use App\Http\Controllers\InformasiController;
 use App\Http\Controllers\Auth\RegisterController;
-use App\Http\Controllers\absensiController;
+use App\Http\Controllers\AbsensiController;
 
 
 
@@ -29,26 +29,26 @@ Route::group(['middleware' => 'auth:sanctum'], function(){
  
 //pegawai
     Route::post('/ApiPegawai',[ApiPegawai::class,'insert']);
-    Route::post('/ApiPegawaiEdit/{id_pegawai}',[pegawaiController::class,'ApiPegawaiEdit']);  
-    Route::get('/pegawaiShow/{id_pegawai}',[pegawaiController::class,'ApiPegawaiShow']); 
+    Route::post('/ApiPegawaiEdit/{id_pegawai}',[PegawaiController::class,'ApiPegawaiEdit']);  
+    Route::get('/pegawaiShow/{id_pegawai}',[PegawaiController::class,'ApiPegawaiShow']); 
    
 
     //absensi
-    Route::get('/ApiAbsensiShow',[absensiController::class,'ApiAbsensiShow']);
+    Route::get('/ApiAbsensiShow',[AbsensiController::class,'ApiAbsensiShow']);
     //riwayat
-    Route::get('/detailAbsensiShow/{id_pegawai}',[absensiController::class,'detailAbsensiShow']);
-    Route::post('/ApiAbsensi',[absensiController::class,'ApiAbsensi']); 
-    Route::put('/ApiAbsensiEdit/{id_absensi}',[absensiController::class,'ApiAbsensiEdit']);  
-    Route::get('/ApiHitung/{id_pegawai}',[absensicontroller::class,'ApiHitung']);
-    Route::get('/time',[absensicontroller::class,'time']);
+    Route::get('/detailAbsensiShow/{id_pegawai}',[AbsensiController::class,'detailAbsensiShow']);
+    Route::post('/ApiAbsensi',[AbsensiController::class,'ApiAbsensi']); 
+    Route::put('/ApiAbsensiEdit/{id_absensi}',[AbsensiController::class,'ApiAbsensiEdit']);  
+    Route::get('/ApiHitung/{id_pegawai}',[AbsensiController::class,'ApiHitung']);
+    Route::get('/time',[AbsensiController::class,'time']);
   
 
 
     //cuti
-    Route::post('/ApiCutiTambah',[cutiController::class,'ApiCutiTambah']);
+    Route::post('/ApiCutiTambah',[CutiController::class,'ApiCutiTambah']);
 
     //jabatan
-    Route::post('/ApiJabatanAdd',[jabatanController::class,'ApiJabatanAdd']);
+    Route::post('/ApiJabatanAdd',[JabatanController::class,'ApiJabatanAdd']);
 
     
 
@@ -60,7 +60,7 @@ Route::group(['middleware' => 'auth:sanctum'], function(){
 
 
     //informasi
-    Route::get('/ApiInformasiShow',[informasiController::class,'ApiInformasiShow']);
+    Route::get('/ApiInformasiShow',[InformasiController::class,'ApiInformasiShow']);
    
  //logout
     Route::get('/logout', [AuthController::class, 'logout']);
